bac392ba53e73312ba4f6aa412d9755badced3cb
Varshith <sriramoju.varshith@outlook.com>
2025-10-29 14:27:32 +0530

    commit

--- InterviewPrograms/Star_Programs/star.java
+++ InterviewPrograms/Star_Programs/star.java
@@ -45,5 +45,21 @@ public class star {
             }
             System.out.println();
         }
+
+
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+        for(int i=n;i>=1;i--){
+            for(int j=i;j<n;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=(2*i-1);j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
